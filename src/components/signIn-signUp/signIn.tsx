22b20faa4d930b2fda34af0d37f53cd9230c7794
Aleksandr Lobkov <das149848@gmail.com>
2022-11-28 16:20:32 +0400

    feat: implement translation of sign in and sign up forms into Russian and switching from Russian to English and vice versa

--- src/components/signIn-signUp/signIn.tsx
+++ src/components/signIn-signUp/signIn.tsx
@@ -12,25 +12,55 @@ import './signIn-signUp.css';
 
 
 function SignIn() {
-  const { loginInputVal, passwordInputVal } = useAppSelector((state) => state.registrwindw)
+  const [ loginInputVal, passwordInputVal, lang ] = useAppSelector((state) => [state.registrwindw.loginInputVal, state.registrwindw.passwordInputVal, state.lang.lang]);
+  const [titleVal, titleChange] = React.useState('');
+  const [loginInscriptionVal, loginInscriptionChange] = React.useState('');
+  const [loginPlaceholderVal, loginPlaceholderChange] = React.useState('');
+  const [passwordInscriptionVal, passowrdInscriptionChange] = React.useState('');
+  const [passwordPlaceholderVal, passowrdPlaceholderChange] = React.useState('');
+  const [signInscriptionButtonVal, signInscriptionButtonChange] = React.useState('');
+  const [registerAskVal, registerAskChange] = React.useState('');
+  const [buttonToSignUpVal, buttonToSignUpChange] = React.useState('');
+
+  React.useEffect(() => {
+    if(lang === "English"){
+      titleChange('Sign In');
+      loginInscriptionChange('Enter login: ');
+      loginPlaceholderChange('Login');
+      passowrdInscriptionChange('Enter password: ');
+      passowrdPlaceholderChange('Password');
+      signInscriptionButtonChange('Sign In');
+      registerAskChange('Not registered?   ');
+      buttonToSignUpChange('Click here to go to Sign Up page');
+    } else {
+      titleChange('Войти');
+      loginInscriptionChange('Введите логин: ');
+      loginPlaceholderChange('Логин');
+      passowrdInscriptionChange('Введите пароль: ');
+      passowrdPlaceholderChange('Пароль');
+      signInscriptionButtonChange('Войти');
+      registerAskChange('Не зарегестрированы?   ');
+      buttonToSignUpChange('Нажмите сюда что бы перейти на старницу регистрации');
+    }
+  }, [lang])
 
   const dispatch = useAppDispatch();
 
   return (
     <div className="authpage">
       <div className="modalsign">
-        <h2>Sign In</h2>
+        <h2>{titleVal}</h2>
         <form>
           <div className="loginInp">
-            <label>Enter login: </label><input placeholder="Login" type="text" autoComplete="username" value={loginInputVal} onChange={(e) => dispatch(loginInputValChange(e.target.value))}></input>
+            <label>{loginInscriptionVal}</label><input placeholder={loginPlaceholderVal} type="text" autoComplete="username" value={loginInputVal} onChange={(e) => dispatch(loginInputValChange(e.target.value))}></input>
           </div>
           <div className="passwordInp">
-            <label>Enter password: </label><input placeholder="Password" type="password" autoComplete="current-password" value={passwordInputVal} onChange={(e) => dispatch(passwordInputValChange(e.target.value))}></input>
+            <label>{passwordInscriptionVal}</label><input placeholder={passwordPlaceholderVal} type="password" autoComplete="current-password" value={passwordInputVal} onChange={(e) => dispatch(passwordInputValChange(e.target.value))}></input>
           </div>
-          <button onClick={signInRequest as unknown as React.MouseEventHandler<HTMLButtonElement>}>Sign In</button>
-          <div className="go-sign-up-tab"><span>Not registered?   </span>
+          <button onClick={signInRequest as unknown as React.MouseEventHandler<HTMLButtonElement>}>{signInscriptionButtonVal}</button>
+          <div className="go-sign-up-tab"><span>{registerAskVal}</span>
           <Link to="/SignUp">
-            <span className="href-sign-up">Click here to go to Sign Up page</span>
+            <span className="href-sign-up">{buttonToSignUpVal}</span>
           </Link>
           </div>
         </form>
--- src/components/signIn-signUp/signUp.tsx
+++ src/components/signIn-signUp/signUp.tsx
@@ -13,28 +13,66 @@ import './signIn-signUp.css';
 
 
 function SignUp() {
-  const { nameInputVal, loginInputVal, passwordInputVal} = useAppSelector((state) => state.registrwindw)
+  const [nameInputVal, loginInputVal, passwordInputVal, lang] = useAppSelector((state) => [state.registrwindw.nameInputVal, state.registrwindw.loginInputVal, state.registrwindw.passwordInputVal, state.lang.lang])
 
   const dispatch = useAppDispatch();
+
+  const [titleVal, titleChange] = React.useState('');
+  const [nameInscriptionVal, nameInscriptionChange] = React.useState('');
+  const [namePlaceholderVal, namePlaceholderChange] = React.useState('');
+  const [loginInscriptionVal, loginInscriptionChange] = React.useState('');
+  const [loginPlaceholderVal, loginPlaceholderChange] = React.useState('');
+  const [passwordInscriptionVal, passowrdInscriptionChange] = React.useState('');
+  const [passwordPlaceholderVal, passowrdPlaceholderChange] = React.useState('');
+  const [signInscriptionButtonVal, signInscriptionButtonChange] = React.useState('');
+  const [signInAskVal, signInAskChange] = React.useState('');
+  const [buttonToSignInVal, buttonToSignInChange] = React.useState('');
+
+  React.useEffect(() => {
+    if(lang === "English"){
+      titleChange('Sign Up');
+      nameInscriptionChange('Enter name: ');
+      namePlaceholderChange('Name');
+      loginInscriptionChange('Enter login: ');
+      loginPlaceholderChange('Login');
+      passowrdInscriptionChange('Enter password: ');
+      passowrdPlaceholderChange('Password');
+      signInscriptionButtonChange('Sign Up');
+      signInAskChange('Already registered?  ');
+      buttonToSignInChange('Click here to go to Sign In page');
+    } else {
+      titleChange('Зарегестрироваться');
+      nameInscriptionChange('');
+      nameInscriptionChange('Введите имя: ');
+      namePlaceholderChange('Имя');
+      loginInscriptionChange('Введите логин: ');
+      loginPlaceholderChange('Логин');
+      passowrdInscriptionChange('Введите пароль: ');
+      passowrdPlaceholderChange('Пароль');
+      signInscriptionButtonChange('Зарегестрироваться');
+      signInAskChange('Уже зарегестрированы?   ');
+      buttonToSignInChange('Нажмите сюда что бы перейти на старницу входа');
+    }
+  }, [lang])
 
   return (
     <div className="authpage">
       <div className="modalsign">
-        <h2>Sign Up</h2>
+        <h2>{titleVal}</h2>
         <form>
           <div className="nameInp">
-            <label>Enter name: </label><input placeholder="Name" type="text" autoComplete="username" value={nameInputVal} onChange={(e) => dispatch(nameInputValChange(e.target.value))}></input>
+            <label>{nameInscriptionVal}</label><input placeholder={namePlaceholderVal} type="text" autoComplete="username" value={nameInputVal} onChange={(e) => dispatch(nameInputValChange(e.target.value))}></input>
           </div>
           <div className="loginInp">
-            <label>Enter login: </label><input placeholder="Login" type="text" autoComplete="username" value={loginInputVal} onChange={(e) => dispatch(loginInputValChange(e.target.value))}></input>
+            <label>{loginInscriptionVal}</label><input placeholder={loginPlaceholderVal} type="text" autoComplete="username" value={loginInputVal} onChange={(e) => dispatch(loginInputValChange(e.target.value))}></input>
           </div>
           <div className="passwordInp">
-            <label>Enter password: </label><input placeholder="Password" type="password" autoComplete="new-password" value={passwordInputVal} onChange={(e) => dispatch(passwordInputValChange(e.target.value))}></input>
+            <label>{passwordInscriptionVal}</label><input placeholder={passwordPlaceholderVal} type="password" autoComplete="new-password" value={passwordInputVal} onChange={(e) => dispatch(passwordInputValChange(e.target.value))}></input>
           </div>
-          <button onClick={signUpRequest as unknown as React.MouseEventHandler<HTMLButtonElement>}>Sign Up</button>
-          <div className="go-sign-up-tab"><span>Already registered?  </span>
+          <button onClick={signUpRequest as unknown as React.MouseEventHandler<HTMLButtonElement>}>{signInscriptionButtonVal}</button>
+          <div className="go-sign-up-tab"><span>{signInAskVal}</span>
             <Link to="/SignIn">
-              <span className="href-sign-up">Click here to go to Sign In page</span>
+              <span className="href-sign-up">{buttonToSignInVal}</span>
             </Link>
           </div>
         </form>
