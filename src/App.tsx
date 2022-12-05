0cf0532a8582f54a9deefe1fc0477ceaa1a528e5
tihohohodka <kikboo1999@gmail.com>
2022-12-05 23:22:28 +0300

    feat: implement ability yo change tasks column

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,10 @@ import {
 import { changeAuth } from "./features/reduxAuth";
 import welcomePicture from "./media/pics/welcome-pic.png";
 import nickPhoto from "./media/pics/nick-photo.jpg";
-<<<<<<< HEAD
 import { BoardPage } from "./components/board-page/board-page";
-=======
-import MainRoute from './components/main-route/main-route';
+import MainRoute from "./components/main-route/main-route";
 const FIFTEEN_MINUTES: number = 900000;
 
->>>>>>> 8c396f5c0c12ff027724c226bbf2726f88e7ee98
 function App() {
   const dispatch = useAppDispatch();
   const reduxAuth = useAppSelector((state) => state.auth);
@@ -72,21 +69,26 @@ function App() {
         <Route
           path="/SignIn"
           element={
-            localStorage.getItem("token") ? <Navigate to="/MainRoute" /> : <SignIn />
+            localStorage.getItem("token") ? (
+              <Navigate to="/MainRoute" />
+            ) : (
+              <SignIn />
+            )
           }
         />
         <Route
           path="/SignUp"
           element={
-            localStorage.getItem("token") ? <Navigate to="/MainRoute" /> : <SignUp />
+            localStorage.getItem("token") ? (
+              <Navigate to="/MainRoute" />
+            ) : (
+              <SignUp />
+            )
           }
         />
         <Route path="/EditProfile" element={<EditProfile />} />
-<<<<<<< HEAD
         <Route path="/Boards" element={<BoardPage />} />
-=======
         <Route path="/MainRoute" element={<MainRoute />} />
->>>>>>> 8c396f5c0c12ff027724c226bbf2726f88e7ee98
       </Routes>
     </BrowserRouter>
   );
--- src/components/board-page/board-page.tsx
+++ src/components/board-page/board-page.tsx
@@ -1,16 +1,26 @@
-import React, { useEffect, ReactNode, useState, useCallback } from "react";
+import React, {
+  useEffect,
+  ReactNode,
+  useState,
+  useCallback,
+  Suspense,
+  useRef,
+} from "react";
 import { useAppSelector, useAppDispatch } from "../../globalStore/globalStore";
 import { Routes, Route, Link } from "react-router-dom";
 import "./board-page.css";
 import type { FC } from "react";
 import { Column, TaskProps } from "./Column";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { taskLoadingChange } from "../../globalStore/taskLoading";
 import { styled } from "../stitches.config";
+import { listenerCancelled } from "@reduxjs/toolkit/dist/listenerMiddleware/exceptions";
+import { Footer } from "../site-parts/footer";
+import { Header } from "../site-parts/header";
 const StyledColumns = styled("div", {
   display: "flex",
-  margin: "10vh auto",
-  marginleft: "15px",
-  height: "60vh",
+  margin: "0px 15px",
+  height: "51vh",
   gap: "8px",
   overflowX: "scroll",
 });
@@ -27,15 +37,23 @@ type getColumnType = {
   boardId: string;
 };
 export function BoardPage() {
-  let boardId: string = "6389245c387211d70eef2af5";
   const [columnModal, setColumnModal] = useState(false);
+  const [taskModal, setTaskModal] = useState(false);
+  const [columnModalChange, setColumnModalChange] = useState(false);
   const [refresh, setRefresh] = useState(999);
   let serverColumns: getColumnType[] = [];
-
+  const [loadingColumns, setLoadingColumns] = useState(false);
   let initialColumns: Record<string, columnType> = {};
-
+  const input1value = useRef<HTMLInputElement>(null);
+  const input2value = useRef<HTMLInputElement>(null);
   const [columns, setColumns] = useState(initialColumns);
+  const boardsRedux = useAppSelector((state) => state.idClickedBoard);
+  const dispatch = useAppDispatch();
+  const taskLoading = useAppSelector((state) => state.taskLoading.loading);
+  //let boardId: string = boardsRedux.idBoradVal;
+  let boardId = "6389245c387211d70eef2af5";
   useEffect(() => {
+    setLoadingColumns(true);
     setTimeout(async () => {
       try {
         const res = await fetch(
@@ -48,7 +66,6 @@ export function BoardPage() {
           }
         );
         serverColumns = await res.json();
-        console.log(serverColumns);
         if (serverColumns[0]) {
           for (let i = 0; i < serverColumns.length; i++) {
             setRefresh(refresh + i);
@@ -64,25 +81,23 @@ export function BoardPage() {
               );
               const tasks: [] = await res2.json();
 
-              console.log(tasks);
               initialColumns[serverColumns[i]._id] = {
                 id: serverColumns[i]._id,
                 list: tasks,
                 title: serverColumns[i].title,
               };
-
-              console.log(initialColumns);
             } catch (err) {
               console.log(err);
             }
           }
         }
         setRefresh(refresh + 1);
+        setLoadingColumns(false);
       } catch (err) {
         console.log(err);
       }
     }, 0);
-  }, [columnModal]);
+  }, [columnModalChange]);
 
   async function createColumn(title: string) {
     const bodyRequest = {
@@ -116,8 +131,6 @@ export function BoardPage() {
         if (res[0]) {
           for (let i = 0; i < res.length; i++) {
             if (res[i].title === title) {
-              console.log("res:");
-              console.log(res[i]);
               const newColumn = {
                 id: res[i]._id,
                 list: [],
@@ -130,6 +143,7 @@ export function BoardPage() {
       } catch (err) {
         console.log(err);
       }
+      setColumnModalChange(!columnModalChange);
       setColumnModal(false);
     } catch (err) {
       console.log(err);
@@ -139,15 +153,17 @@ export function BoardPage() {
     title: string,
     column: string,
     description: string,
-    userId: string
+    userId: string,
+    col: columnType
   ) {
     const bodyRequest = {
       title: title,
       order: 0,
       description: description,
       userId: userId,
-      users: ["1", "2"],
+      users: [],
     };
+    console.log(bodyRequest);
     try {
       const res = await fetch(
         `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${column}/tasks`,
@@ -160,16 +176,32 @@ export function BoardPage() {
           body: JSON.stringify(bodyRequest),
         }
       );
-
       const data = await res.json();
-      console.log(data);
-      console.log(bodyRequest);
+      const newTask: TaskProps = {
+        boardId: boardId,
+        columnId: column,
+        text: description,
+        order: 0,
+        title: title,
+        userId: userId,
+        users: [],
+        _id: data._id,
+      };
+
+      const newList: TaskProps[] = col.list.concat(newTask);
+      const newCol: columnType = {
+        id: col.id,
+        list: newList,
+        title: col.title,
+      };
+      // Update the state
+      setColumns((state) => ({ ...state, [newCol.id]: newCol }));
     } catch (err) {
       console.log(err);
     }
   }
 
-  const onDragEnd = ({ source, destination }: DropResult) => {
+  const onDragEnd = async ({ source, destination }: DropResult) => {
     // Make sure we have a valid destination
     if (destination === undefined || destination === null) return null;
 
@@ -195,13 +227,59 @@ export function BoardPage() {
       newList.splice(destination.index, 0, start.list[source.index]);
 
       // Then create a new copy of the column object
+
+      for (let i = 0; i < start.list.length; i++) {
+        console.log(start.list);
+        dispatch(taskLoadingChange(true));
+        try {
+          await fetch(
+            `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${start.id}/tasks/${start.list[i]._id}`,
+            {
+              method: "DELETE",
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      for (let j = 0; j < newList.length; j++) {
+        const bodyRequest = {
+          title: newList[j].title,
+          order: 0,
+          description: newList[j].text,
+          userId: "user",
+          users: ["1", "2"],
+        };
+        try {
+          const data = await fetch(
+            `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${start.id}/tasks`,
+            {
+              method: "POST",
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(bodyRequest),
+            }
+          );
+          const res = await data.json();
+          newList[j]._id = res._id;
+        } catch (err) {
+          console.log(err);
+        }
+      }
       const newCol = {
         id: start.id,
         list: newList,
         title: start.title,
       };
-
       // Update the state
+      dispatch(taskLoadingChange(false));
       setColumns((state) => ({ ...state, [newCol.id]: newCol }));
       return null;
     } else {
@@ -210,6 +288,51 @@ export function BoardPage() {
       const newStartList = start.list.filter(
         (_: any, idx: number) => idx !== source.index
       );
+      for (let i = 0; i < start.list.length; i++) {
+        console.log(start.list);
+        dispatch(taskLoadingChange(true));
+        try {
+          await fetch(
+            `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${start.id}/tasks/${start.list[i]._id}`,
+            {
+              method: "DELETE",
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      for (let j = 0; j < newStartList.length; j++) {
+        const bodyRequest = {
+          title: newStartList[j].title,
+          order: 0,
+          description: newStartList[j].text,
+          userId: "user",
+          users: ["1", "2"],
+        };
+        try {
+          const data = await fetch(
+            `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${start.id}/tasks`,
+            {
+              method: "POST",
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(bodyRequest),
+            }
+          );
+          const res = await data.json();
+          newStartList[j]._id = res._id;
+        } catch (err) {
+          console.log(err);
+        }
+      }
 
       // Create a new start column
       const newStartCol = {
@@ -217,13 +340,60 @@ export function BoardPage() {
         list: newStartList,
         title: start.title,
       };
-
+      const newEndList: TaskProps[] = [];
       // Make a new end list array
-      const newEndList = end.list;
+      newEndList.push(...end.list);
 
       // Insert the item into the end list
       newEndList.splice(destination.index, 0, start.list[source.index]);
-
+      console.log("newEndlength" + newEndList.length);
+      console.log(end.list);
+      if (end.list[0]) {
+        for (let i = 0; i < end.list.length; i++) {
+          try {
+            await fetch(
+              `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${end.id}/tasks/${end.list[i]._id}`,
+              {
+                method: "DELETE",
+                headers: {
+                  Authorization: "Bearer " + localStorage.getItem("token"),
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+      for (let j = 0; j < newEndList.length; j++) {
+        const bodyRequest = {
+          title: newEndList[j].title,
+          order: 0,
+          description: newEndList[j].text,
+          userId: "user",
+          users: [],
+        };
+        console.log("bodyRequest");
+        console.log(bodyRequest);
+        try {
+          const data = await fetch(
+            `https://kanban-server-production.up.railway.app/boards/${boardId}/columns/${end.id}/tasks`,
+            {
+              method: "POST",
+              headers: {
+                Authorization: "Bearer " + localStorage.getItem("token"),
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(bodyRequest),
+            }
+          );
+          const res = await data.json();
+          newEndList[j]._id = res._id;
+        } catch (err) {
+          console.log(err);
+        }
+      }
       // Create a new end column
       const newEndCol = {
         id: end.id,
@@ -237,69 +407,121 @@ export function BoardPage() {
         [newStartCol.id]: newStartCol,
         [newEndCol.id]: newEndCol,
       }));
+      dispatch(taskLoadingChange(false));
       return null;
     }
   };
 
   return (
     <>
-      <button
-        onClick={() => {
-          setColumnModal(true);
-        }}
-      >
-        Create column
-      </button>
-      {columnModal && (
-        <div className="modal-window-column ">
-          <label htmlFor="ColumnName">Title:</label>
-          <input type="text" id="columnModalTitleInput"></input>
-          <div className="modal-button-holder">
-            <button
-              onClick={async () => {
-                const input: HTMLInputElement = document.querySelector(
-                  "#columnModalTitleInput"
-                ) as HTMLInputElement;
+      <Header></Header>
+      <main>
+        <button
+          onClick={() => {
+            setColumnModal(true);
+          }}
+        >
+          Create column
+        </button>
+        {columnModal && (
+          <div className="modal-window-column ">
+            <label htmlFor="ColumnName">Title:</label>
+            <input type="text" id="columnModalTitleInput"></input>
+            <div className="modal-button-holder">
+              <button
+                onClick={async () => {
+                  const input: HTMLInputElement = document.querySelector(
+                    "#columnModalTitleInput"
+                  ) as HTMLInputElement;
 
-                if (input) {
-                  await createColumn(input.value);
-                }
-              }}
-            >
-              create Column
-            </button>
-            <button
-              onClick={() => {
-                setColumnModal(false);
-              }}
-            >
-              Cancel
-            </button>
+                  if (input) {
+                    await createColumn(input.value);
+                  }
+                }}
+              >
+                create Column
+              </button>
+              <button
+                onClick={() => {
+                  setColumnModal(false);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      <DragDropContext onDragEnd={onDragEnd}>
-        <StyledColumns>
-          {Object.values(columns).map((col) => (
-            <Column col={col} key={col.id} />
-          ))}
-        </StyledColumns>
-      </DragDropContext>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {loadingColumns ? (
+            <div>Loading...</div>
+          ) : (
+            <StyledColumns>
+              {Object.values(columns).map((col, index) => (
+                <div className="column-holder" key={col.id}>
+                  <Column col={col} key={col.id} />
 
-      <button
-        onClick={async () => {
-          setRefresh(refresh + 1);
-          await createTask(
-            "title56",
-            "638d7bf1387211d70eef79bd",
-            "description",
-            "User"
-          );
-        }}
-      >
-        create task
-      </button>
+                  {taskModal && (
+                    <div className="modal-window-column ">
+                      <label htmlFor="TaskName">Title:</label>
+                      <input
+                        type="text"
+                        id="taskInput"
+                        ref={input1value}
+                      ></input>
+                      <label htmlFor="TaskDesc">Title:</label>
+                      <input
+                        type="text"
+                        id="taskInput2"
+                        ref={input2value}
+                      ></input>
+                      <div className="modal-button-holder">
+                        <button
+                          onClick={() => {
+                            console.log("value " + input1value.current!.value);
+                            console.log(col);
+                            setTimeout(async () => {
+                              console.log("start");
+
+                              await createTask(
+                                input1value.current!.value,
+                                col.id,
+                                input2value.current!.value,
+                                "user",
+                                col
+                              );
+
+                              setTaskModal(false);
+                            });
+                          }}
+                        >
+                          Create
+                        </button>
+                        <button
+                          onClick={() => {
+                            setTaskModal(false);
+                          }}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                  <button
+                    id={col.id + "btn"}
+                    onClick={async () => {
+                      setTaskModal(true);
+                    }}
+                  >
+                    Create Task
+                  </button>
+                </div>
+              ))}
+            </StyledColumns>
+          )}
+        </DragDropContext>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
